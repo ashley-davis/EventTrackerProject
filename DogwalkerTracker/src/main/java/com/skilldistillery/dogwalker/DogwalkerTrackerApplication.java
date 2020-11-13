9160aa2ecd0cb96f7c03d381816c5a599b286b5c
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-11-13 16:41:56 -0700

    stub boot project with JPA project dependency

---
+++ DogwalkerTracker/src/main/java/com/skilldistillery/dogwalker/DogwalkerTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dogwalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogwalkerTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogwalkerTrackerApplication.class, args);
+	}
+
+}
---
+++ DogwalkerTracker/src/test/java/com/skilldistillery/dogwalker/DogwalkerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dogwalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogwalkerTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
