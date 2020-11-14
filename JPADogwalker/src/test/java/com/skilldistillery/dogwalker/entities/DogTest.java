c62883e288577eb7d658373a531b48ade3a18829
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-11-13 21:30:51 -0700

    cleaning up tests

--- JPADogwalker/src/test/java/com/skilldistillery/dogwalker/entities/DogTest.java
+++ JPADogwalker/src/test/java/com/skilldistillery/dogwalker/entities/DogTest.java
@@ -1,4 +1,4 @@
-package com.skilldistillery.dogwalker;
+package com.skilldistillery.dogwalker.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DogTest {
+	
 private static EntityManagerFactory emf;
 private EntityManager em;
 private Dog cowboy;
